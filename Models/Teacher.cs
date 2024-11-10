9c78120fe668b858c0a3f9da7266f9a5a603e79a
Leila1996 <bsze3huslei@vasvari.org>
2024-11-10 13:46:32 +0100

    Teacher class létrehozása

---
+++ Models/Teacher.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Xml.Linq;
+
+namespace StudentProject.Models
+{
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int ExperienceYears { get; set; }
+        public bool IsHeadOfDepartment { get; set; }
+    }
+
+        public Teacher(string name, string subject, int experienceYears, bool isHeadOfDepartment)
+        {
+            Name = name;
+            Subject = subject;
+            ExperienceYears = experienceYears;
+            IsHeadOfDepartment = isHeadOfDepartment;
+        }
+        public override string ToString()
+        {
+            return $"{Name}, Tantárgy: {Subject}, Tapasztalat: {ExperienceYears} év, Tanszékvezető: {(IsHeadOfDepartment ? "Igen" : "Nem")}";
+        }
+}
