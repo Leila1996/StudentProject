f1c4a2cbe42e51815620baa8da8ad75d9ed93b1b
Leila1996 <bsze3huslei@vasvari.org>
2024-11-10 18:01:24 +0100

    administrator class készítése

--- Models/Administrator.cs
+++ Models/Administrator.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace StudentProject.Models
 {
-    internal class Administrator
+    public class Administrator
     {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string TaskDescription { get; set; }
+
+        public Administrator(string name, string department, string taskDescription)
+        {
+            Name = name;
+            Department = department;
+            TaskDescription = taskDescription;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, Osztály: {Department}, Feladat: {TaskDescription}";
+        }
     }
 }
